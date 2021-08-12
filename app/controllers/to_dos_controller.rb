1632d750b35fd601ca7572173e5f2a1b850a65b0
Jack Hubert <jphubert93@gmail.com>
2021-08-12 17:21:02 -0600

    build a create action in the to_dos_controller in order to persist new to_do items to the backend from the to_do_form; user_story #2 complete

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -5,4 +5,16 @@ class ToDosController < ApplicationController
 
         render json: @todos
     end
+
+    def create
+        @to_do = ToDo.create(
+            title: params[:title],
+            content: params[:content],
+            urgent: params[:urgent],
+            done: params[:done]
+        )
+
+        render json: @to_do, status: :created
+    end
+
 end
