eb0b988b1ec7e64b1c029d88ec2528f429ca1293
Jack Hubert <jphubert93@gmail.com>
2021-08-18 11:44:30 -0600

    updated User controller, creating strong params & defining 'create' method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
 
+    before_action: 
+
     def create
-        # Some code
+        @user = User.new(user_params)
+
+        if @user.valid?
+            User.save(@user)
+            render json: @user, status: :created
+        else
+            render json: { error: "Username has already been taken" }, status: :not_acceptable
+        end
     end
 
-    
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
