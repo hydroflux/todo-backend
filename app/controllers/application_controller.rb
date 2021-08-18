e5783bd5860466559acbc6d661d6ffdf46d464fe
Jack Hubert <jphubert93@gmail.com>
2021-08-18 17:14:30 -0600

    user story #6 => as a user, I can saccess the application when logged in; created login function in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+    def login
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            token = JWT.encode({user_id: @user.id}, "some secret")
+            render json: { user: @user, token: @token}, status :accepted
+        else
+            render json: { message: "Invalid username or password."}
+        end
+    end
+
 end
